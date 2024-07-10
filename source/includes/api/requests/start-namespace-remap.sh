f0747c19467fce5ed7a14d6aa1d0fab1ad6e21c9
Kenneth P. J. Dyer <kenneth.dyer@mongodb.com>
2024-07-10 15:32:43 -0400

    Adds request/response example

--- source/includes/api/requests/start-namespace-remap.sh
+++ source/includes/api/requests/start-namespace-remap.sh
@@ -4,8 +4,14 @@ curl -X POST "http://localhost:27182/api/v1/start" --data '
       "destination": "cluster1",
       "namespaceRemap": [
         {
-           "from": { "database": "accounts", "collection": "new_england" },
-           "to": { "database": "accounts", "collection": "us_northeast" }
+           "from": {
+              "database": "accounts",
+              "collection": "new_england"
+           },
+           "to": {
+              "database": "accounts",
+              "collection": "us_northeast"
+           }
         }
       ]
    } '
